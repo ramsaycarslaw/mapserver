b42121c431881dadf173139fcd326d6605d6f56d
Ramsay <ramsaycarslaw@icloud.com>
2023-03-24 15:49:05 +0000

    actual docs

--- services/main.go
+++ services/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	fs := http.FileServer(http.Dir("../public"))
+
+	// check if env port is set
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("✅ Running map server on")
+	log.Println("<======================>")
+	log.Println("🚀 http://localhost:" + port)
+
+	err := http.ListenAndServe(":"+port, fs)
+
+	// handle the error if exists
+	if err != nil {
+		log.Fatal(err)
+	}
 }
